d64da077e9990936c2629531ce30e648d3667cb6
uvarovalexandr <uvarov.alexandr89@gmail.com>
2024-12-01 21:18:03 +0200

    fix

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { ChatDatabaseService } from '../database/chat-database.service';
-import { UpdateMessageDto } from '../message/dto/update-message.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
 
 @Injectable()
