dc1fdad886d116f396bdf2252b770400e61a8190
uvarovalexandr <uvarov.alexandr89@gmail.com>
2024-12-05 14:00:14 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   app.enableCors({
     origin: 'http://localhost:3001',
